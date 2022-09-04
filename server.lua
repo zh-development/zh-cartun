c18c99c97c29080ceeac2f5578ee6c35ff653199
zh_development <73235871+zh-development@users.noreply.github.com>
2022-09-04 17:03:20 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Commands.Add('tu', "max tun car",{}, false, function(source)
-   TriggerEvent('zh-cartun:client:tun', source)
+   TriggerClientEvent('zh-cartun:client:tun', source)
 end, 'god')
